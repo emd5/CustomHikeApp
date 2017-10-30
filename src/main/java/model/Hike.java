331878f2540652e79325b8c372b115f60d5cc1c0
emd5 <emahoney5@mail.greenriver.edu>
2017-10-30 09:58:40 -0700

    "Initial commit Hike class"

---
+++ src/main/java/model/Hike.java
@@ -0,0 +1,33 @@
+package model;
+
+
+import java.util.List;
+
+public class Hike {
+
+    private static List<Hike> hikeList;
+
+    public void addHike(String name , int duration, String location){
+
+    }
+
+    public Hike getHike(String name){
+        return null;
+    }
+
+    public int getAverageHeartRateByHikeName(){
+        return 0;
+    }
+
+    public int getAverageSteps(){
+        return 0;
+    }
+
+    public String[] getHikeNames(){
+        return null;
+    }
+
+
+
+
+}
