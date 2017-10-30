e3d1eb999d93d1b55e3d7b00b6eba8549d236e7a
emd5 <emahoney5@mail.greenriver.edu>
2017-10-30 09:33:56 -0700

    Initial commit

---
+++ src/main/java/controller/HikeController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class HikeController extends Application{
+
+
+
+    @Override
+    public void start(Stage primaryStage){
+
+
+    }
+
+
+}
